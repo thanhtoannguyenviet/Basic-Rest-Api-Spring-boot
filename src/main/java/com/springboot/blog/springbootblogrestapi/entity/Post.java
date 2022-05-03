babb12c7aba33998d1999a2083c58903e223417e
Nguyen Viet Thanh Toan <toandeveloper99@gmail.com>
2022-05-03 10:18:43 +0700

    Init Project

--- src/main/java/com/springboot/blog/springbootblogrestapi/entity/Post.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/entity/Post.java
@@ -1,2 +1,27 @@
-package com.springboot.blog.springbootblogrestapi.entity;public class Post {
+package com.springboot.blog.springbootblogrestapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "content", nullable = false)
+    private String content;
 }
