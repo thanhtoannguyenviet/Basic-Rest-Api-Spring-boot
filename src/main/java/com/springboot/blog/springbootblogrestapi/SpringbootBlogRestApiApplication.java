aaed4f78162bed31b00c1f0ffb4aeb1fd458bd69
Nguyen Viet Thanh Toan <toandeveloper99@gmail.com>
2022-05-09 14:45:55 +0700

    Update for deploy

--- src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,12 +1,16 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import com.springboot.blog.springbootblogrestapi.entity.Role;
+import com.springboot.blog.springbootblogrestapi.respository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication  implements CommandLineRunner{
 
     @Bean
     public ModelMapper modelMapper(){
@@ -15,6 +19,22 @@ public class SpringbootBlogRestApiApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+    }
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+
     }
 
 }
